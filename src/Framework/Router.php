1b6a1d9ca6d610cee9ed42c3af1a81486bfeb64a
DESKTOP-7DB5H5Q\Jaques <jaques.strydom@gmail.com>
2024-05-28 11:49:54 +0200

    Added Home Controller. Updated bootstrap and Router to dispatch the controller

---
+++ src/Framework/Router.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+namespace Framework;
+
+class Router
+{
+    private array $routes = [];
+
+    public function add(string $method, string $path, array $controller) {
+        $path = $this->normalizePath($path);
+        $this->routes[] = [
+            'path' => $path,
+            'method' => strtoupper($method),
+            'controller' => $controller,
+        ];
+    }
+
+    private function normalizePath(string $path): string {
+        $path = trim($path, '/');
+        $path =  "/{$path}/";
+        return preg_replace('#[/]{2,}#', '/', $path);
+    }
+
+    public function dispatch(string $path, string $method) {
+        $path = $this->normalizePath($path);
+        $method = strtoupper($method);
+
+        echo $path . $method;
+    }
+}
