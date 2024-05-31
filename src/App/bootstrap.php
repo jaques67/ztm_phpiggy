b6cdaf3431124fa6b6a6a554b0e669cdedb2e62f
DESKTOP-7DB5H5Q\Jaques <jaques.strydom@gmail.com>
2024-05-31 10:18:15 +0200

    Moved registering routes to a function and autoload the function

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 require __DIR__."/../../vendor/autoload.php";
 
-use App\Controllers\{AboutController, HomeController};
 use Framework\App;
+
+use function App\Config\registerRoutes;
 
 $app = new App();
 
-$app->get('/', [HomeController::class, 'home']);
-$app->get('/about', [AboutController::class, 'about']);
+registerRoutes($app);
 
 return $app;
